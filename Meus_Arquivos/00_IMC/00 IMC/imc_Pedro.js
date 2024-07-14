1eccf62fc54954b950b41e389447897e23c518fc
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-07-14 10:16:42 -0300

    Realocação das pastas
    Realizado a realocação das pastas

--- Meus_Arquivos/00_IMC/00 IMC/imc_Pedro.js
+++ Meus_Arquivos/00_IMC/00 IMC/imc_Pedro.js
@@ -1,15 +1,14 @@
-
 const calcular = document.getElementById('calcular');
 
 
 function imc () {
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;    
+    const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
     if (nome !== '' && altura !== '' && peso !== '') {
-        
+
         const valorIMC = (peso / (altura * altura)).toFixed(1);
 
         let classificacao = '';
@@ -29,13 +28,10 @@ function imc () {
         }
 
         resultado.textContent = `${nome}, seu IMC é ${valorIMC} e você está ${classificacao}.`;
-
     } else {
-        resultado.textContent = 'Preencha todos os campos!'
+        resultado.textContent = `Preencha todos os campos!`
     }
-
 }
-
 
 
 calcular.addEventListener('click', imc);
