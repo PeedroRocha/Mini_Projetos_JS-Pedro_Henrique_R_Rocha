598f1b7213f45ce4d213c0db62593013aec5b533
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-08-03 09:29:17 -0300

    Refazendo exercício
    Refazendo o exercício, pois o anterior não funcionou

--- Arquivos_Professor/01 Lampada/lampada.js
+++ Arquivos_Professor/01 Lampada/lampada.js
@@ -1,10 +1,2 @@
-const turnOn = document.getElementById ('turnOn');
-const turnOff = document.getElementById ('turnOff');
-const lamp = ducument.getElementById ('lamp');
-
-function lampOn () {
-    lamp.src = './img/ligada.jpg'; /*paramos aqui,não está funcionando a troca de imagem ( 9 minutos ) */
-}
-
-
-turnOn.addEventListener ( 'click', lampOn );
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' ); /*Paramos aqui, 7:54 min */ 
