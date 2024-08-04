c4f87c80c778e82344a6f335bd02922a3fa19ff3
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-08-04 11:21:27 -0300

    Continuado o exercício
    Realizando junto com o professor

--- Arquivos_Professor/01 Lampada/lampada.js
+++ Arquivos_Professor/01 Lampada/lampada.js
@@ -1,2 +1,29 @@
 const turnOn = document.getElementById ( 'turnOn' );
-const turnOff = document.getElementById ( 'turnOff' ); /*Paramos aqui, 7:54 min */ 
+const turnOff = document.getElementById ( 'turnOff' );
+const lamp = document.getElementById ( 'lamp' )
+
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1    
+}
+
+function lampOn () {
+    if ( !isLampBroken () ) {
+        lamp.src = './img/ligada.jpg';
+    } 
+}
+function lampOff () {
+    if ( !islampBroken () ) {
+        lamp.src = './img/desligada.jpg'; /* paramos aqui, 19 min do vídeo, na inspeção do google apontou o erro aqui nesta linha */
+    }
+}
+function lampBroken () {
+    lamp.src = './img/quebrada.jpg';    
+}
+
+turnOn.addEventListener ( 'click', lampOn );
+turnOff.addEventListener ( 'click', lampOff );
+lamp.addEventListener ( 'mouseover', lampOn );
+lamp.addEventListener ( 'mouseleave', lampOff );
+lamp.addEventListener ( 'dblclick', lampBroken );
+
+
