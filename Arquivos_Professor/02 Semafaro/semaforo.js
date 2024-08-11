712d2ad8a1bf0f797bd28e19b6af2c4414a7323b
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-08-11 10:56:18 -0300

    Vídeo do professor
    Assistindo vídeo para aprender a logica js

---
+++ Arquivos_Professor/02 Semafaro/semaforo.js
@@ -0,0 +1,6 @@
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
+
+
+
+buttons.addEventListener('click', trafficLight); /* Paramos aqui, 6 min */
