6f651b1290f16721869224e2030b9e23754b7359
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-08-18 10:37:35 -0300

    Semafaro Prof
    Código do semáforo finalizado.
    Realizado junto com o professor

--- Arquivos_Professor/02 Semafaro/semaforo.js
+++ Arquivos_Professor/02 Semafaro/semaforo.js
@@ -1,6 +1,37 @@
 const img = document.getElementById( 'img' );
 const buttons = document.getElementById( 'buttons' );
+let colorIndex = 0;
+let intervalId = null;
 
+const trafficLight = ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
 
+const nextIndex = () => {
+    if (colorIndex < 2) {
+        colorIndex++;
+    }else {
+        colorIndex = 0;    
+    }   
+}
 
-buttons.addEventListener('click', trafficLight); /* Paramos aqui, 6 min */
+const changeColor = () => {
+    const colors = ['red','yellow','green'];
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic = () => {
+    clearInterval ( intervalId );    
+}
+
+const turnOn = {
+    'red':       () => img.src = './img/vermelho.png',
+    'yellow':    () => img.src = './img/amarelo.png',
+    'green':     () => img.src = './img/verde.png',
+    'automatic': () => intervalId = setInterval( changeColor, 1000 ),
+}
+
+buttons.addEventListener('click', trafficLight);
