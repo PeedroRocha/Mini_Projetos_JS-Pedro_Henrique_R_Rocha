7970f9e916e781c19d82537283589f5065eeac86
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-09-08 08:09:30 -0300

    Continuação
    Continuação da cópia do código

--- Arquivos_Professor/04 DRUMKIT/index.js
+++ Arquivos_Professor/04 DRUMKIT/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const sons = {
+    'A': 'bomm.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav',
+}
+
+const criarDiv = (texto) => {
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+}
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+exibir(sons);
+
+
+
+document.getElementById('container')
+        .addEventListener('click', ativarDiv); /* Paramos em 34 min*/
