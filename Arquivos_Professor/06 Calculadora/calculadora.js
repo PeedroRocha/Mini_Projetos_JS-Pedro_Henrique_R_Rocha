f8edf71db3d968b69f3ed279450c6aa258e0a186
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-10-09 14:05:19 -0300

    Continuação JS
    Linha 35, JS

--- Arquivos_Professor/06 Calculadora/calculadora.js
+++ Arquivos_Professor/06 Calculadora/calculadora.js
@@ -2,3 +2,34 @@
 
 const display = document.getElementById('display');
 const numeros = document.querySelectorAll('[id*=tecla]');
+const operadores = document.querySelectorAll('[id*=operador]');
+
+let novoNumero = true;
+let operador;
+let numeroAnterior;
+
+const operacaoPendente = () => operador !== undefined;
+
+const calcular = () => {
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent.replace('.','').replace(',', '.'));
+        novoNumero = true;
+        const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`);
+        atualizarDisplay(resultado);
+    }
+};
+
+const atualizarDisplay = (texto) => {
+    if (novoNumero) {
+        display.textContent = texto.toLocaleString('BR');
+        novoNumero = false;
+    } else {
+        display.textContent += texto.toLocaleString('BR');
+    }
+    document.querySelector('#igual').focus();
+};
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
+
+// paramos aqui
