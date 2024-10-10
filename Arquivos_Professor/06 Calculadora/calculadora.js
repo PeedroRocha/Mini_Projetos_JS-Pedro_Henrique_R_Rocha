a13c7fd2e0e22b7dfce500f46ca59b16f200c176
PeedroRocha <136763248+PeedroRocha@users.noreply.github.com>
2024-10-10 06:55:32 -0300

    Contiação JS
    Continuação JS

--- Arquivos_Professor/06 Calculadora/calculadora.js
+++ Arquivos_Professor/06 Calculadora/calculadora.js
@@ -32,4 +32,49 @@ const atualizarDisplay = (texto) => {
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
 numeros.forEach((numero) => numero.addEventListener('click', inserirNumero));
 
+const selecionarOperador = (evento) => {
+    if (!novoNumero) {
+        calcular();
+        novoNumero = true;
+        operador = evento.target.textContent;
+        numeroAnterior = parseFloat(display.textContent.replace('.','').replace(',', '.'));
+    }
+};
+operadores.forEach((operador) => 
+    operador.addEventListener('click', selecionarOperador)
+);
+
+const ativarIgual = () => {
+    calcular();
+    operador = undefined;
+};
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+const limparDisplay = () => (display.textContent = '');
+document
+    .getElementById('limparDisplay')
+    .addEventListener('click', limparDisplay);
+
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
+    novoNumero = true;
+    numeroAnterior = undefined;
+};
+document
+    .getElementById('limparCalculo')
+    .addEventListener('click', limparCalculo);
+
+const removerUltimoNumero = () =>
+    (display.textContent = display.textContent.slice(0, -1));
+document
+    .getElementById('backspace')
+    .addEventListener('click', removerUltimoNumero);
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+};
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
 // paramos aqui
